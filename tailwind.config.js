c0051efb45d191fe4bc51e851a813c1f97e52335
tharunadhithya <tharun2839@gmail.com>
2024-03-01 17:07:19 +0530

    final code

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,19 @@ module.exports = {
     './components/**/*.{html,js}',
   ],
   theme: {
+    screens: {
+      'xs': '320px',
+     
+      'sm': '480px',
+
+      'md': '640px',
+    
+
+      'lg': '1024px',
+
+
+      'xl': '1280px',
+    },
     extend: {},
   },
   plugins: [],
